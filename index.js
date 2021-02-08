4995128fcb89675182843d0a653c59da3b0566e9
Naseem <naseem.siddiqui2000@gmail.com>
2021-02-08 12:48:49 +0530

    naseem commit 4

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://naseem:naseem123@cluster0.iq1il.mongodb.net/naseem?retryWrites=true&w=majority",{
+   useNewUrlParser: true
+}, function(error){
+    if(error){
+       console.log(error);
+    }
+    else{
+       console.log("Connected to the Database");
+    }
+});
 
 app.get('/', function(req, res){
    res.send("Hello world!");
